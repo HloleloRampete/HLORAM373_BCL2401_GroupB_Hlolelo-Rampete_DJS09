9dbb1c5b8420f1c875602645b7bfa81a64e8cce1
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-09 23:27:55 +0200

    "Added impots to index.ts"

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {
+    showReviewTotal,
+    populateUser,
+    showDetails,
+    getTopTwoReviews,
+  } from "./utils";
+  import { Permissions, LoyaltyUser } from "./enums";
+  import { Review, Property } from "./interfaces";
+  import MainProperty from "./classes";
