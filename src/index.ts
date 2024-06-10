b67382edd395bcf7d8df505a72f91bdb3b4ccb2d
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-10 10:07:39 +0200

    "Removed imports and replaced with inline code and data"

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import {
-    showReviewTotal,
-    populateUser,
-    showDetails,
-    getTopTwoReviews,
-  } from "./utils";
-  import { Permissions, LoyaltyUser } from "./enums";
-  import { Review, Property } from "./interfaces";
-  import MainProperty from "./classes";
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+const reviews = [
+  {
+      name: 'Sheia',
+      stars: 5,
+      loyaltyUser: true,
+      date: '01-04-2021'
+  },
+  {
+      name: 'Andrzej',
+      stars: 3,
+      loyaltyUser: false,
+      date: '28-03-2021'
+  },
+  {
+      name: 'Omar',
+      stars: 4,
+      loyaltyUser: true,
+      date: '27-03-2021'
+  },
+]
+
+function showReviewTotal (value : number) {
+  reviewTotalDisplay.innerHTML = 'review total' + value.toString()
+}
+
+showReviewTotal(reviews.length)
